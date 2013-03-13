d6f4574f146b84f1d094413b1035017d7a230b75
Tomasz Curylo <tcurylo@ioki.com.pl>
2013-03-13 14:45:12 +0100

    zmiana

--- Twig/FunnyExtension.php
+++ Twig/FunnyExtension.php
@@ -21,12 +21,18 @@ class FunnyExtension extends \Twig_Extension
     {
         return array(
             'smile' => new \Twig_Function_Method($this, 'smileFunction'),
+            'copyright' => new \Twig_Function_Method($this, 'copyrightFunction'),
         );
     }
 
     public function smileFunction($number = 5)
     {
         return str_repeat(':-)', $number);
+    }
+
+    public function copyrightFunction()
+    {
+        return '&copy;';
     }
 
     public function getName()
